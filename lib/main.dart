f493f8b8a192d40f200bc7448ea7acff1fe50f09
KeitaFukue <k.fukue.it@gmail.com>
2021-04-25 01:30:54 +0900

    second commmit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'SimpleApp',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('SimpleApp'),
+        ),
+        body: Center(
+          child: Text(
+            'SimpleApp'
+          ),
+        ),
+      ),
+
+    );
+  }
+}
